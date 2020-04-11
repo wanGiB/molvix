948bc8f948a318caa69ff8387a5d5c0fdcf46bb4
Wan Clem <codeflippa@gmail.com>
2020-04-11 00:24:23 -0700

    Bug Fixes and Code Improvements

--- app/src/main/java/com/molvix/android/utils/ConnectivityUtils.java
+++ app/src/main/java/com/molvix/android/utils/ConnectivityUtils.java
@@ -2,7 +2,9 @@ package com.molvix.android.utils;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
+import android.os.Build;
 
 import com.molvix.android.components.ApplicationLoader;
 
@@ -10,8 +12,25 @@ public class ConnectivityUtils {
     public static boolean isDeviceConnectedToTheInternet() {
         ConnectivityManager connectivityManager = (ConnectivityManager) ApplicationLoader.getInstance().getSystemService(Context.CONNECTIVITY_SERVICE);
         if (connectivityManager != null) {
-            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-            return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                NetworkCapabilities capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.getActiveNetwork());
+                if (capabilities != null) {
+                    if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                        return true;
+                    } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                        return true;
+                    } else return capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET);
+                }
+            } else {
+                try {
+                    NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+                    if (activeNetworkInfo != null && activeNetworkInfo.isConnected()) {
+                        return true;
+                    }
+                } catch (Exception ignored) {
+
+                }
+            }
         }
         return false;
     }
